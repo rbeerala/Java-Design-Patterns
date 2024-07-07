1219a22b6c024180376891ded0deb74b2eee25e2
Ravi Bee <rtrbeerala@gmail.com>
2024-07-07 13:33:42 -0400

    Print change to main

--- src/main/java/org/decoratorPattern/Main.java
+++ src/main/java/org/decoratorPattern/Main.java
@@ -6,6 +6,6 @@ import org.decoratorPattern.pizzaDecorator.MushroomTopping;
 public class Main {
     public static void main(String[] args) {
         Pizza pizza = new MushroomTopping(new ExtraCheeseTopping(new VegeDelight()));
-        System.out.println(pizza.getCost());
+        System.out.println("Pizza Cost is : "+pizza.getCost());
     }
 }
